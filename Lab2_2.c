678a477df6729565966079b0a6ee10b645d2f2c9
Litoru <puwarin2001@gmail.com>
2021-02-11 15:18:44 +0700

    แก้ราคาในหน่วยวิ

--- Lab2_2.c
+++ Lab2_2.c
@@ -7,20 +7,20 @@ void main()
     float time,addt;
     scanf("%f",&time);
     float min=(int)time,sec=(time-(int)time)*100;
-    printf("min=%f ,sec=%f /n",min,sec);
+    //printf("min=%f ,sec=%f\n",min,sec);
 
     switch(pro)
     {
         case 'A':
             if(min>200.0)
                 addt=min-200.0;
-            printf("%.2f",(addt*3.0)+199.0+(sec/60.0*199) );
+            printf("%.2f",(addt*3.0)+199.0+(sec/60*3.0) );
             break;
 
         case 'B':
             if(min>400.0)
                 addt=min-400.0;
-            printf("%.2f",(addt*2.0)+299.0+(sec/60.0*299) );
+            printf("%.2f",(addt*2.0)+299.0+(sec/60*2.0) );
             break;
     }
 
