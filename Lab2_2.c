350e788c1dbfc4ecf7c3c5bcdc59f824d4275d3b
Litoru <puwarin2001@gmail.com>
2021-02-11 15:12:50 +0700

    แก้ code 2-2

--- Lab2_2.c
+++ Lab2_2.c
@@ -4,8 +4,25 @@ void main()
 {
     char pro;
     scanf("%c",&pro);
-    float time;
+    float time,addt;
     scanf("%f",&time);
+    float min=(int)time,sec=(time-(int)time)*100;
+    printf("min=%f ,sec=%f /n",min,sec);
+
+    switch(pro)
+    {
+        case 'A':
+            if(min>200.0)
+                addt=min-200.0;
+            printf("%.2f",(addt*3.0)+199.0+(sec/60.0*199) );
+            break;
+
+        case 'B':
+            if(min>400.0)
+                addt=min-400.0;
+            printf("%.2f",(addt*2.0)+299.0+(sec/60.0*299) );
+            break;
+    }
 
 
 }
